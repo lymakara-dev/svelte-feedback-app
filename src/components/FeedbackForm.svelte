666af254dfdc6e97a0e11e6b81540b5999db8f2b
lymakara <lymakara07012004@gmail.com>
2024-08-22 01:38:19 +0700

    add some functions

---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,75 @@
+<script>
+  import Button from "./Button.svelte";
+  import Card from "./Card.svelte";
+  import RatingSelect from "./RatingSelect.svelte";
+
+  let text = ''
+  let rating = 10
+  let btnDisable = true
+  let min = 10
+  let message
+  
+  const handleSelect = (e) => rating = e.detail
+
+  const handleInput = () => {
+    if(text.trim().length <= min){
+        message = `Text must be at least ${min} charaters`
+        btnDisable = true
+    }else{
+        message = null
+        btnDisable = false
+    }
+  }
+</script>
+
+<Card>
+    <header>
+        <h2>How would you rate your service with us?</h2>
+    </header>
+    <form>
+        <!-- Rating section -->
+         <RatingSelect on:rating-select={handleSelect}/>
+         <div class="input-group">
+            <input type="text" on:input={handleInput} bind:value={text} placeholder="Tell us something that keeps you coming back">
+            <Button disabled={btnDisable} type="submit">Send</Button>
+        </div>
+        {#if message}
+            <div class="message">
+                {message}
+            </div>
+        {/if}
+    </form>
+</Card>
+
+<style>
+    header {
+        max-width: 400px;
+        margin: auto;
+        text-align: center;
+    }
+
+    .input-group {
+        display: flex;
+        flex-direction: row;
+        border: 1px #ccc solid;
+        padding: 8px 10px;
+        border-radius: 8px;
+        margin: 15px;
+    }
+
+    input {
+        flex-grow: 2;
+        border: none;
+        font-size: 16px;
+    }
+
+    input:focus {
+        outline: none;
+    }
+
+    .message {
+        padding-top: 10px;
+        text-align: center;
+        color: purple;
+    }
+</style>
